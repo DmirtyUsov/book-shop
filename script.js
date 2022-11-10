00149c6d139a687e6da26048af6e624ee4316d6b
Dmitry Usov <dmitry.usov@yahoo.com>
2022-11-10 21:14:21 +0300

    init:

---
+++ script.js
@@ -0,0 +1,3 @@
+document.documentElement.lang = 'en';
+document.head.innerHTML = '<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><title>Book Shop</title>';
+console.log(document.body);
