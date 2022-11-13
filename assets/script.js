597b38af639ff47b82a954b59cff5ce16537ea40
Dmitry Usov <dmitry.usov@yahoo.com>
2022-11-13 15:35:13 +0300

    feat: header

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,15 @@
 document.documentElement.lang = 'en';
+
 document.head.innerHTML = `
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../../assets/style.css">
     <title>Book Shop</title>`;
+
+
+let header = document.createElement('header')
+header.insertAdjacentHTML('afterbegin', '<h1>Book Shop</h1>')
+document.body.append(header);
 
 fetch('../../assets/books.json') //path to the file with json data
     .then(response => {
