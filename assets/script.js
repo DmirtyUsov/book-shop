35934599f363fdb41735b08cfb83b192556809b8
Dmitry Usov <dmitry.usov@yahoo.com>
2022-11-19 12:19:04 +0300

    feat: drag&drop

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
 let books = [];
+let draggedNow = null;
+
 fetch('../../assets/books.json') // path to the file with json data
     .then(response => {
         return response.json();
@@ -7,9 +9,33 @@ fetch('../../assets/books.json') // path to the file with json data
         console.log(data);
         books = data;
     });
-const togglePopup = (idx) =>{
+// functions for EventListners
+
+const togglePopup = (idx) => {
     console.log('press', idx);
     document.getElementById(`popup-${idx}`).classList.toggle("active"); 
+}
+
+const addBookToOrder = (idx) => {
+    console.log('add', idx);
+
+    let fragment = new DocumentFragment();
+    let book = document.createElement('div');
+    book.classList.add('book');
+    let title = document.createElement('h5');
+    title.classList.add('book-title')
+    title.textContent = books[idx].title;
+
+    book.append(title);
+    fragment.append(book);
+    let order = document.getElementById('cart');
+    order.insertAdjacentElement('beforeend', book);
+    draggedNow = null;
+}
+
+const startDrag = (idx) => {
+    console.log('Drag', idx);
+    draggedNow = idx;
 }
 
 document.documentElement.lang = 'en';
@@ -56,6 +82,7 @@ setTimeout(() => { // wait for data from fetch
         cover.classList.add('book-cover');
         cover.src = item.imageLink;
         cover.alt = `Cover for ${item.title}`;
+        cover.addEventListener("dragstart", function(){startDrag(idx)});
 
         let popup = document.createElement('div');
         popup.classList.add('popup');
@@ -88,6 +115,7 @@ setTimeout(() => { // wait for data from fetch
         let add = document.createElement('button');
         add.classList.add('book-add');
         add.textContent = 'Add to Order';
+        add.addEventListener('click', function(){addBookToOrder(idx)});
 
         book.append(popup);
         book.append(title);
@@ -103,8 +131,15 @@ setTimeout(() => { // wait for data from fetch
 
     divMy = document.createElement('div')
     divMy.classList.add('cart');
-    mainMy.append(divMy);
+    divMy.id = 'cart';
+    divMy.addEventListener("dragover", (event) => {
+        // prevent default to allow drop
+        event.preventDefault();
+        console.log('dragover');
+      });
+    divMy.addEventListener("drop", function(){addBookToOrder(draggedNow)})
     divMy.innerHTML = "<h2>My Order<h2>";
+    mainMy.append(divMy);
 
     fragment.append(mainMy);
 
